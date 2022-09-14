175028d6ecfdd416f007d1f5fc6d9fd6fc6bca04
EsmeraldaBenzadon <110538279+EsmeraldaBenzadon@users.noreply.github.com>
2022-09-14 11:59:17 -0300

    comienzo de abecedario

---
+++ WindowsFormsApp2/Letra.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Letra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Letra));
+            this.reproductor = new AxWMPLib.AxWindowsMediaPlayer();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.reproductor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reproductor
+            // 
+            this.reproductor.Enabled = true;
+            this.reproductor.Location = new System.Drawing.Point(147, 158);
+            this.reproductor.Name = "reproductor";
+            this.reproductor.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("reproductor.OcxState")));
+            this.reproductor.Size = new System.Drawing.Size(557, 304);
+            this.reproductor.TabIndex = 0;
+            this.reproductor.Enter += new System.EventHandler(this.AxWindowsMediaPlayer1_Enter);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1_FileOk);
+            // 
+            // Letra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(860, 490);
+            this.Controls.Add(this.reproductor);
+            this.DoubleBuffered = true;
+            this.Name = "Letra";
+            this.Text = "Letra";
+            this.Shown += new System.EventHandler(this.Letra_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.reproductor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer reproductor;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ WindowsFormsApp2/Letra.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Letra : Form
+    {
+        string letra;
+
+        public Letra(string le)
+        {
+            InitializeComponent();
+            letra = le;
+        }
+
+        private void AxWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Letra_Shown(object sender, EventArgs e)
+        {
+            string dirProyecto = AppContext.BaseDirectory;
+            dirProyecto = dirProyecto.Substring(0, dirProyecto.Length - 10);
+            reproductor.URL = dirProyecto + "Letras\\" + letra + ".mp4";
+            reproductor.Ctlcontrols.play();
+        }
+    }
+}
