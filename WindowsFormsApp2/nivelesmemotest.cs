c5faa5759415e8dce9e3d3f81d83358466fe6112
EsmeraldaBenzadon <47872003@est.ort.edu.ar>
2022-10-20 12:40:26 -0300

    memotest 222

---
+++ WindowsFormsApp2/nivelesmemotest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class nivelesmemotest : Form
+    {
+        public nivelesmemotest()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
